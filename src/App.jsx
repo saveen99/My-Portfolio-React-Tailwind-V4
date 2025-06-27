a7e98bb243362c1cf5910a6a175c64683cf71d48
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2025-06-27 21:45:19 +0530

    tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 function App() {
 
   return (
-    <>
-      <p className='text-red-500'>Hello world..!</p>
-    </>
+    <div className="relative h-screen">
+  <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+    SAVEEN MADURANGA
+  </div>
+</div>
   )
 }
 
