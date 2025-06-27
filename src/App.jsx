807c1bd53549250a53fc854ef002cd6dfad6a09a
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2025-06-27 21:46:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <div className="relative h-screen">
-  <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+  <div className="absolute text-accent top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
     SAVEEN MADURANGA
   </div>
 </div>
