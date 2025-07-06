cd34f69e450a896e0f6ce53849b62771c13252a0
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2025-07-06 20:39:19 +0530

    update skills
    Co-Authored-By: Saveen Maduranga II <165816828+saveen993@users.noreply.github.com>

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -14,8 +14,9 @@ const SkillCard = ({
     classes
 }) => {
   return (
-    <div className="">
-        <figure className="">
+    <div className={'flex items-center gap-3 ring-2 ring-inset ring-zinc-50/10 rounded-2xl p-3 hover:bg-zinc-800 transition-colors group ' + classes}>
+        <figure className="bg-zinc-700/50 rounded-lg overflow-hidden w-12 h-12 p-2
+        group-hover:bg-zinc-900 transition-colors">
             <img 
             src={imgSrc}
             width={32}
@@ -26,7 +27,7 @@ const SkillCard = ({
         <div>
             <h3>{label}</h3>
 
-            <p className="">
+            <p className="text-zinc-400 text-sm">
                 {desc}
             </p>
         </div>
