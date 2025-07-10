548799f77532fdeb208096f102d6fb973ad4f584
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2025-07-10 17:39:13 +0530

    update contact section
    Co-Authored-By: Saveen Maduranga II <165816828+saveen993@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@
 */
 
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Skill from "./components/Skill";
@@ -18,6 +19,7 @@ const App = () => {
         <About />
         <Skill />
         <Work />
+        <Contact />
       </main>
     </>
   )
