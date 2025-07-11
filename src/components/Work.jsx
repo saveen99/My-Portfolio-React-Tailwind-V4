1250cacf8b7fb18006bc00c374db2d22c02fe14a
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2025-07-11 15:51:44 +0530

    update files
    Co-Authored-By: Saveen Maduranga II <165816828+saveen993@users.noreply.github.com>

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -9,39 +9,39 @@ import ProjectCard from "./ProjectCard";
 
 const works = [
   {
-    imgSrc: '/images/project-1.jpg',
+    imgSrc: '/images/project-1.jpeg',
     title: 'AASL - Training Management System',
     tags: ['Laravel', 'Talwind', 'MySQL'],
     projectLink: 'https://musify-5al0.onrender.com/'
   },
   {
-    imgSrc: '/images/project-2.jpg',
-    title: 'Free stock photo app',
-    tags: ['API', 'SPA'],
+    imgSrc: '/images/project-2.jpeg',
+    title: 'AASL - Key Management System Mobile App',
+    tags: ['Kotlin', 'SQLite', 'Android Studio'],
     projectLink: 'https://pixstock-official.vercel.app/'
   },
   {
-    imgSrc: '/images/project-3.jpg',
-    title: 'Recipe app',
-    tags: ['Development', 'API'],
+    imgSrc: '/images/project-3.png',
+    title: 'Hardware Management Web System (WPL Trading)',
+    tags: ['JavaScript', 'PHP', 'MySQL'],
     projectLink: ''
   },
   {
-    imgSrc: '/images/project-4.jpg',
-    title: 'Real state website',
-    tags: ['Web-design', 'Development'],
+    imgSrc: '/images/project-4.png',
+    title: 'E-commerce website',
+    tags: ['React', 'Tailwind CSS'],
     projectLink: 'https://github.com/codewithsadee-org/wealthome'
   },
   {
-    imgSrc: '/images/project-5.jpg',
-    title: 'eCommerce website',
-    tags: ['eCommerce', 'Development'],
+    imgSrc: '/images/project-5.png',
+    title: 'Car Service website ( Sasik Service Station )',
+    tags: ['React', 'Tailwind CSS', 'Firebase'],
     projectLink: 'https://github.com/codewithsadee/anon-ecommerce-website'
   },
   {
-    imgSrc: '/images/project-6.jpg',
-    title: 'vCard Personal portfolio',
-    tags: ['Web-design', 'Development'],
+    imgSrc: '/images/project-6.png',
+    title: 'Restaurant Landing Page',
+    tags: ['React', 'CSS'],
     projectLink: 'https://github.com/codewithsadee/vcard-personal-portfolio'
   },
 ];
