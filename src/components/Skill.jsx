dac7d2ec3f53c6b3044762f199f6099a202df886
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2025-07-11 12:14:25 +0530

    GSAP scrollTrigger
    Co-Authored-By: Saveen Maduranga II <165816828+saveen993@users.noreply.github.com>

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -54,11 +54,11 @@ const Skill = () => {
   return (
     <section className="section">
         <div className="container">
-            <h2 className="headline-2">
+            <h2 className="headline-2 reveal-up">
                 Essential Tools I use
             </h2>
             <p className="text-zinc-400 mt-3 mb-8
-            max-w-[50ch]">
+            max-w-[50ch] reveal-up">
                 Discover the powerful tools and technologies I use to create exceptional, high-performing websites & applications.
             </p>
 
@@ -70,6 +70,7 @@ const Skill = () => {
                             imgSrc={imgSrc}
                             label={label}
                             desc={desc}
+                            classes="reveal-up"
                         />
                     ))
                 }
