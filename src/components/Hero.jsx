211093a13b8f03eaf5c9e41339b4175a5dbdbc6e
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2025-07-13 17:18:07 +0530

    bug-fix
    Co-Authored-By: Saveen Maduranga II <165816828+saveen993@users.noreply.github.com>

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -60,7 +60,7 @@ const Hero = () => {
             <div className="hidden lg:block">
                 <figure className="w-[400px] max-w-[480px] ml-auto bg-gradient-to-t
                 from-sky-400 via-25% via-sky-400/40 to-65% rounded-[60px] overflow-hidden">
-                    <img src="/images/hero-banner.png" 
+                    <img src="/images/hero-banner.jpg" 
                     width={656}
                     height={800}
                     alt="Saveen Maduranga" 
