b1eef2a0289c6f78b2a6744f34efbcc5e34f7603
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2025-07-17 09:47:27 +0530

    Update Skill.jsx
    Co-Authored-By: Saveen Maduranga II <165816828+saveen993@users.noreply.github.com>

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -52,7 +52,10 @@ const skillItem = [
 
 const Skill = () => {
   return (
-    <section className="section">
+    <section 
+      id="skill"
+      className="section"
+    >
         <div className="container">
             <h2 className="headline-2 reveal-up">
                 Essential Tools I use
