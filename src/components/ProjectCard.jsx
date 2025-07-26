39354523f4856816f3cd038234c4124594c33c39
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2025-07-26 10:46:37 +0530

    update projects
    Co-Authored-By: Saveen Maduranga II <165816828+saveen993@users.noreply.github.com>

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -43,12 +43,13 @@ const ProjectCard = ({
                 </div>
             </div>
 
-            <div className="w-11 h-11 rounded-lg grid place-items-center bg-sky-400 text-zinc-950 shrink-0 ">
+            <div className="w-10 h-10 rounded-lg grid place-items-center bg-sky-400 text-zinc-950 shrink-0 ">
                 <span 
                     className="material-symbols-rounded"
-                    aria-hidden="true">
-                        arrow_outward
-                    </span>
+                    aria-hidden="true"
+                >
+                    arrow_outward
+                </span>
             </div>
         </div>
 
